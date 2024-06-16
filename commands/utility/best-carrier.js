13e3ff1e173aad67c0cf7a3bf49d279af58abfda
Maerwan <hotwraith4803@gmail.com>
2024-06-17 01:10:32 +0200

    [NEW] best carrier command

--- commands/utility/best-carrier.js
+++ commands/utility/best-carrier.js
@@ -2,13 +2,13 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('best carrier')
+		.setName('best-carrier')
 		.setDescription('Replies with name of the best carrier!'),
     
     carriers : ["P.T.N. Tykie", "P.T.N. Sketchy", "P.T.N. Gaïa's Refuge", "PTN Bay of my heart", "P.T.N. BANKRUPTCY", "PTN Afterlife", "P.T.N. Valeria", "P.T.N. Angry Gnome", "P.T.N. the Peregrine", "P.T.N. Legendary"],
 	async execute(interaction) {
         number = Math.floor(Math.random() * 11);
         carrier = carriers[number]
-		await interaction.reply('Pong!');
+		await interaction.reply(`The best carreier is: ${carrier}`);
 	},
 };
