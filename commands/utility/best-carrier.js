97e0fb082bcbfa3f41a8294ab598c67c5a182c7f
Maerwan <hotwraith4803@gmail.com>
2024-06-17 01:13:26 +0200

    [FIX] best carrier command

--- commands/utility/best-carrier.js
+++ commands/utility/best-carrier.js
@@ -4,11 +4,11 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('best-carrier')
 		.setDescription('Replies with name of the best carrier!'),
-    
-    carriers : ["P.T.N. Tykie", "P.T.N. Sketchy", "P.T.N. Gaïa's Refuge", "PTN Bay of my heart", "P.T.N. BANKRUPTCY", "PTN Afterlife", "P.T.N. Valeria", "P.T.N. Angry Gnome", "P.T.N. the Peregrine", "P.T.N. Legendary"],
+
 	async execute(interaction) {
+        const carriers = ["P.T.N. Tykie", "P.T.N. Sketchy", "P.T.N. Gaïa's Refuge", "PTN Bay of my heart", "P.T.N. BANKRUPTCY", "PTN Afterlife", "P.T.N. Valeria", "P.T.N. Angry Gnome", "P.T.N. the Peregrine", "P.T.N. Legendary"],
         number = Math.floor(Math.random() * 11);
         carrier = carriers[number]
-		await interaction.reply(`The best carreier is: ${carrier}`);
+		await interaction.reply(`The best carrier is: ${carrier}`);
 	},
 };
