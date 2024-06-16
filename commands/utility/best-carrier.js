5435d609abe3f1f5c5ecb1f13eaa59e865f256d8
Maerwan <hotwraith4803@gmail.com>
2024-06-17 01:17:39 +0200

    [FIX] best carrier command

--- commands/utility/best-carrier.js
+++ commands/utility/best-carrier.js
@@ -6,8 +6,8 @@ module.exports = {
 		.setDescription('Replies with name of the best carrier!'),
 
 	async execute(interaction) {
-        const carriers = ["P.T.N. Tykie", "P.T.N. Sketchy", "P.T.N. Gaïa's Refuge", "PTN Bay of my heart", "P.T.N. BANKRUPTCY", "PTN Afterlife", "P.T.N. Valeria", "P.T.N. Angry Gnome", "P.T.N. the Peregrine", "P.T.N. Legendary"],
-        number = Math.floor(Math.random() * 11);
+        const carriers = ["P.T.N. Tykie", "P.T.N. Sketchy", "P.T.N. Gaïa's Refuge", "PTN Bay of my heart", "P.T.N. Bankruptcy", "PTN Afterlife", "P.T.N. Valeria", "P.T.N. Angry Gnome", "P.T.N. the Peregrine", "P.T.N. Legendary"],
+        number = Math.floor(Math.random() * 10);
         let carrier = carriers[number]
 		await interaction.reply(`The best carrier is: ${carrier}`);
 	},
